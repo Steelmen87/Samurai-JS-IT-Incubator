4011c5a29a6469921f7868629a37a073e47d86a2
Steel <https://github.com/Steelmen87/>
2022-07-13 09:20:21 +0300

    lesson 1-9

---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>Css</li>
+                <li>HTML</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
