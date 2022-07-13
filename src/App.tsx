a001ad654d5a05786957b5a3355b94d782013837
Steel <https://github.com/Steelmen87/>
2022-07-13 13:20:04 +0300

    lesson 10-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import {Technologies} from "./Technologies";
+
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://cs13.pikabu.ru/avatars/3066/x3066923-1218757612.png ' alt=''/>
+            </header>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Message</a></div>
+                <div><a>New</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://gidfon.com/prev/8919.jpg' alt=''/>
+                </div>
+                <div>Ava + description</div>
+                <div>My post
+                    <div>
+                        New Post
+                    </div>
+                    <div>Post1</div>
+                    <div>Post1</div>
+                    <div>Post1</div>
+                </div>
+            </div>
         </div>
     );
 }
