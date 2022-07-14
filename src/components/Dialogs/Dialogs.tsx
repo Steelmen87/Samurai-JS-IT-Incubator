a900f8be7ff2b25b57e0a21231b19b1c03da0377
Steel <https://github.com/Steelmen87/>
2022-07-14 13:39:37 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,29 @@ import style from './Dialogs.module.css'
 type PropsType = {}
 const Dialogs = (props: PropsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={`${style.dialog} ${style.active}`}>
+                    Dimych
+                </div>
+            <div className={style.dialog}>
+                    Andrey
+                </div>
+            <div className={style.dialog}>
+                    Sveta
+                </div>
+            <div className={style.dialog}>
+                    Valera
+                </div>
+            <div className={style.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you</div>
+                <div className={style.message}>Yo yO</div>
+            </div>
         </div>
     )
 }
