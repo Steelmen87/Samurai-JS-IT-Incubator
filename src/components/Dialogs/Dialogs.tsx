bc82b6c2b9b183e22416c745d312bde13875a3ab
Steel <https://github.com/Steelmen87/>
2022-07-16 06:24:51 +0300

    lesson 31 refMessage.current?.value

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {RefObject} from 'react'
 import style from './Dialogs.module.css'
 import {Dialog} from "./DialogItem/Dialog";
 import Message from "./Message/Message";
@@ -10,10 +10,16 @@ type PropsDialogType = {
     messageData: Array<MessageType>
 }
 const Dialogs = (props: PropsDialogType) => {
-    let {dialogData,messageData} = props
+    let {dialogData, messageData} = props
 
-    let dialogsElements = dialogData.map(el => <Dialog name={el.name} id={el.id}avatar={el.avatar} />)
+    let dialogsElements = dialogData.map(el => <Dialog name={el.name} id={el.id} avatar={el.avatar}/>)
     let messagesData = messageData.map(el => <Message message={el.message}/>)
+    let refMessage:RefObject<HTMLTextAreaElement> = React.createRef();
+    let addMessage = () => {
+        let message = refMessage.current?.value
+        alert(message)
+    }
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
@@ -22,6 +28,11 @@ const Dialogs = (props: PropsDialogType) => {
             <div className={style.messages}>
                 {messagesData}
             </div>
+            <div className={style.addPost}>
+                <textarea ref={refMessage}></textarea>
+                <button onClick={addMessage}>add message</button>
+            </div>
+
         </div>
     )
 }
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {RefObject} from "react";
 import style from './MyPosts.module.css'
 import Post from "./Post/Post";
 
@@ -20,13 +20,20 @@ const MyPosts = (props: PropsType) => {
             message={el.message}
             likeCounter={el.likeCounter}
         />)
+    let newPostElement:RefObject<HTMLTextAreaElement> = React.createRef();
+
+    let addPost = () =>{
+        let text = newPostElement.current?.value
+        alert(text)
+    }
+
     return (
         <div className={style.postsBlock}>
             <h3>My post</h3>
             <div>
-                <textarea/>
+                <textarea ref={newPostElement}></textarea>
                 <br/>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={style.posts}>
                 {postElement}
