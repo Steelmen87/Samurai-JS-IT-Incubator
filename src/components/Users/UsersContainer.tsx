074205a7ad804f2d1e5e3e40b58a27deb5df4016
Steel <Steelmen87@bk.ru>
2022-10-24 15:31:02 +0300

    83 done

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -20,7 +20,7 @@ import {
     getIsFetching,
     getPageSize,
     getTotalUsersCount,
-    getUsers
+    getUsers, getUsersSuper
 } from "../../redux/usersSelectors";
 
 
@@ -32,7 +32,7 @@ export type ResponseDatatype = {
 
 const mapStateToProps = (state: AppStateType): UsersType => {
     return {
-        users: getUsers(state),
+        users: getUsersSuper(state),
         pageSize: getPageSize(state),
         totalUsersCount: getTotalUsersCount(state),
         currentPage: getCurrentPage(state),
